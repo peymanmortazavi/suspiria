b0ccccf9d07beaf5030b728908bf174566c2d3e4
Peyman Mortazavi <peyman.mortazavi@infoscoutinc.com>
2019-02-07 01:20:01 -0500

    Added mongoose and main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "mongoose/mongoose.h"
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  cout << "Hello from C++ world inside a docker image." << endl;
   return 0;
 }
