f67394d8655a20e026a08ef939864aa6589740c6
Peyman Mortazavi <peymanmortazavi@users.noreply.github.com>
2019-02-16 09:49:56 -0500

    Added bash scripts to build and start suspiria dev box.
    Signed-off-by: Peyman Mortazavi <peyman.mortazavi@infoscoutinc.com>

--- docker/development/start.sh
+++ docker/development/start.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-docker build -t cpp-dev-tools .
-docker run -p 0.0.0.0:2222:22 -p 0.0.0.0:5000:5000 --name cpp-dev cpp-dev-tools
+docker run -p 0.0.0.0:2222:22 -p 0.0.0.0:5000:5000 --cap-add=SYS_PTRACE --name suspiria-dev suspiria-dev-image
